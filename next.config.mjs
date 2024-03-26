bd280b526755a08dd9702a908b7eb882b8bfa188
rendarCP <tjddnr0217@gmail.com>
2024-03-27 00:47:05 +0900

    feat: vanilla extract 설정 추가 및 token값 정의

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+// const withVanillaExtract = createVanillaExtractPlugin();
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// module.exports = withVanillaExtract(nextConfig);
+
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
---
+++ src/styles/variants/token.ts
@@ -0,0 +1,114 @@
+export const neutral = {
+  100: "#030303",
+  90: "#1A1A1A",
+  80: "#333333",
+  70: "#4D4D4D",
+  60: "#666666",
+  50: "#808080",
+  40: "#999999",
+  30: "#B3B3B3",
+  20: "#CCCCCC",
+  10: "#E6E6E6",
+  5: "#F3F3F3",
+  white: "#FFFFFF",
+};
+
+export const primary = {
+  100: "#3E74FE",
+  90: "#5282FF",
+  80: "#6590FE",
+  70: "#789EFF",
+  60: "#8BACFE",
+  50: "#9FBAFF",
+};
+
+export const system = {
+  warning: "#FD3D51",
+  success: "#1CD14F",
+};
+
+export const title = {
+  h1: {
+    fontWeight: "bold",
+    fontSize: 40,
+  },
+  h2: {
+    fontWeight: "bold",
+    fontSize: 32,
+  },
+  h3: {
+    fontWeight: "bold",
+    fontSize: 28,
+  },
+  h4: {
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  h5: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+};
+
+export const content = {
+  bold20: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  medium20: {
+    fontWeight: 500,
+    fontSize: 20,
+  },
+  regular20: {
+    fontWeight: 400,
+    fontSize: 20,
+  },
+  bold18: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  medium18: {
+    fontWeight: 500,
+    fontSize: 18,
+  },
+  regular18: {
+    fontWeight: 400,
+    fontSize: 18,
+  },
+  bold16: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  medium16: {
+    fontWeight: 500,
+    fontSize: 16,
+  },
+  regular16: {
+    fontWeight: 400,
+    fontSize: 16,
+  },
+  bold14: {
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  medium14: {
+    fontWeight: 500,
+    fontSize: 14,
+  },
+  regular14: {
+    fontWeight: 400,
+    fontSize: 14,
+  },
+  bold12: {
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  medium12: {
+    fontWeight: 500,
+    fontSize: 12,
+  },
+  regular12: {
+    fontWeight: 400,
+    fontSize: 12,
+  },
+};
