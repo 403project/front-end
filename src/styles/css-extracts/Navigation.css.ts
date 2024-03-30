3fd04fc4607424f4370212f167a9a15cbfdc59e3
jaewoong2 <jwisgenius@naver.com>
2024-03-30 15:13:36 +0900

    feat: 메인 페이지 구현

--- src/styles/css-extracts/Navigation.css.ts
+++ src/styles/css-extracts/Navigation.css.ts
@@ -9,15 +9,18 @@ export const LogoContainer = style({
 
 export const Naviation = style({
   display: "flex",
-  padding: "22px 26px",
+  padding: "16px 24px",
   alignItems: "center",
   borderBottom: "1px solid",
   borderColor: vars.strokeLine["10"],
   position: "fixed",
+  backdropFilter: "blur(5px)",
   top: 0,
+  left: 0,
   width: "100%",
-  backgroundColor: vars.neutral[0],
+  backgroundColor: "#ffffff59",
   zIndex: 100,
+  justifyContent: "space-between",
 });
 
 export const LogoTitle = style({
@@ -25,4 +28,37 @@ export const LogoTitle = style({
   ...content.medium16,
   fontFamily: fontFamily,
   marginLeft: 6,
+});
+
+export const NavigationGroups = style({
+  display: "flex",
+  gap: "20px",
+  marginLeft: 76,
+  color: vars.neutral["90"],
+  ...content.regular16,
+  fontFamily: "Pretendard",
+});
+
+export const NavigationRightGroups = style({
+  display: "flex",
+  gap: "20px",
+  color: vars.neutral["90"],
+  ...content.regular16,
+  alignItems: "center",
+  fontFamily: "Pretendard",
+});
+
+export const NavigationButton = style({
+  padding: "14px 12px",
+  border: "1px solid",
+  color: vars.neutral["90"],
+  ...content.regular14,
+  borderColor: vars.strokeLine["10"],
+  borderRadius: 6,
+});
+
+export const NaviationPrimary = style({
+  backgroundColor: vars.primary["100"],
+  color: vars.neutral["0"],
+  border: "0px",
 });
