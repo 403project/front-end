5e0365738a6fd1f7c5855e880a2ccfd9f1cd070f
rendarCP <tjddnr0217@gmail.com>
2024-03-31 01:34:50 +0900

    feat: 프로젝트 목록 부분 api 연동 작업 및 기타 오류 수정

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -11,10 +11,29 @@ import { ReactNode, useEffect, useRef, useState } from "react";
 import VoteCard from "./_component/VoteCatd";
 import Ranking from "./_component/Ranking";
 import Navigation from "../_component/Navigation";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
+import loading from '../../../public/loading.svg';
+import Image from "next/image";
 
-export default function Vote(): ReactNode {
+export default function Vote() {
+  const [lists, setLists] = useState([]);
+  const searchParams = useSearchParams()
+  
   const [activeCardIndex, setActiveCardIndex] = useState(-1);
   const voteCardRefs = useRef<any>([]);
+
+  useEffect(() => {
+    axios.get(`https://api.byulbyul.store/polls/${searchParams.get('pollid')}`)
+    .then(res => {
+      const { projects } = res.data;
+      setLists(projects);
+    })
+    .catch(err => {
+      console.log('err', err);
+    })
+  }, [])
+
 
   useEffect(() => {
     const handleClickOutside = (event: any) => {
@@ -32,31 +51,52 @@ export default function Vote(): ReactNode {
     <>
       <Navigation />
       <main className={voteContainer}>
-        <div className={voteTitle}>3월의 프로젝트</div>
+        <div className={voteTitle}>{`${searchParams.get('pollid')}월의 프로젝트`}</div>
         <div>
-          <select className={voteSelect}>
+          {/* <select className={voteSelect}>
             <option defaultChecked>전체</option>
             <option>등록일 순</option>
             <option>가나다 순</option>
             <option>조회수 높은 순</option>
             <option>조회수 낮은 순</option>
-          </select>
+          </select> */}
           <div className={voteWrapper}>
-            <div className={voteCardBox}>
-              {Array.from({ length: 5 }).map((_, index) => {
-                return (
-                  <VoteCard
-                    key={index}
-                    ref={(el: any) => (voteCardRefs.current[index] = el)}
-                    isActive={index === activeCardIndex}
-                    onMoreClick={(e: any) => {
-                      e.stopPropagation();
-                      setActiveCardIndex(index === activeCardIndex ? -1 : index);
-                    }}
-                    setActiveCardIndex={setActiveCardIndex}
-                  />
-                );
-              })}
+            <div>
+              {
+                lists.length > 0 ? 
+                  <div className={voteCardBox}>
+                    {lists.map((list, index) => {
+                      return (
+                        <VoteCard
+                          key={index}
+                          ref={(el: any) => (voteCardRefs.current[index] = el)}
+                          isActive={index === activeCardIndex}
+                          onMoreClick={(e: any) => {
+                            e.stopPropagation();
+                            setActiveCardIndex(index === activeCardIndex ? -1 : index);
+                          }}
+                          setActiveCardIndex={setActiveCardIndex}
+                          list={list}
+                        />
+                      );
+                    })}
+                  </div>
+                :
+                <div
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    gap: "10px",
+                  }}
+                >
+                  <Image src={loading} width={320} height={320} alt="로고" />
+                  <div style={{ fontWeight: "bold", fontSize: 24, fontFamily: "Pretendard" }}>조금만 기다려주세요...</div>
+                </div>
+              }
             </div>
             <div className={rankingBox}>
               <Ranking />
